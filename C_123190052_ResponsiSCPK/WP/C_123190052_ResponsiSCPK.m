b49e52fa0caa2eb09eafa675aaec55900c2a5f43
FaturRumahan <81752796+faturrumahan@users.noreply.github.com>
2021-06-25 20:08:45 +0700

    Add files via upload

---
+++ C_123190052_ResponsiSCPK/WP/C_123190052_ResponsiSCPK.m
@@ -0,0 +1,125 @@
+function varargout = C_123190052_ResponsiSCPK(varargin)
+% C_123190052_RESPONSISCPK MATLAB code for C_123190052_ResponsiSCPK.fig
+%      C_123190052_RESPONSISCPK, by itself, creates a new C_123190052_RESPONSISCPK or raises the existing
+%      singleton*.
+%
+%      H = C_123190052_RESPONSISCPK returns the handle to a new C_123190052_RESPONSISCPK or the handle to
+%      the existing singleton*.
+%
+%      C_123190052_RESPONSISCPK('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in C_123190052_RESPONSISCPK.M with the given input arguments.
+%
+%      C_123190052_RESPONSISCPK('Property','Value',...) creates a new C_123190052_RESPONSISCPK or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before C_123190052_ResponsiSCPK_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to C_123190052_ResponsiSCPK_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help C_123190052_ResponsiSCPK
+
+% Last Modified by GUIDE v2.5 25-Jun-2021 17:07:04
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @C_123190052_ResponsiSCPK_OpeningFcn, ...
+                   'gui_OutputFcn',  @C_123190052_ResponsiSCPK_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before C_123190052_ResponsiSCPK is made visible.
+function C_123190052_ResponsiSCPK_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to C_123190052_ResponsiSCPK (see VARARGIN)
+
+% Choose default command line output for C_123190052_ResponsiSCPK
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes C_123190052_ResponsiSCPK wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = C_123190052_ResponsiSCPK_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in tampil.
+function tampil_Callback(hObject, eventdata, handles)
+% hObject    handle to tampil (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+opts = detectImportOptions('Real_estate.xlsx');
+opts.SelectedVariableNames = (1:5);
+data = readmatrix('Real_estate.xlsx', opts);
+set(handles.tabel1,'data',data,'visible','on'); %membaca file dan menampilkan pada tabel
+
+
+% --- Executes on button press in proses.
+function proses_Callback(hObject, eventdata, handles)
+% hObject    handle to proses (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+opts = detectImportOptions('Real_estate.xlsx');
+opts.SelectedVariableNames = (2:5);
+data = readmatrix('Real_estate.xlsx', opts); %membaca file
+k=[0,0,1,0]; %cost atau benefit
+w=[3,5,4,1]; %bobot kriteria
+
+%tahapan pertama, perbaikan bobot
+[m n]=size (data); %inisialisasi ukuran x
+w=w./sum(w); %membagi bobot per kriteria dengan jumlah total seluruh bobot
+
+%tahapan kedua, melakukan perhitungan vektor(S) per baris (alternatif)
+for j=1:n,
+    if k(j)==0, w(j)=-1*w(j);
+    end;
+end;
+for i=1:m,
+    S(i)=prod(data(i,:).^w);
+end;
+
+V= S/sum(S) %perhitungan nilai vektor V
+
+opts = detectImportOptions('Real_estate.xlsx');
+opts.SelectedVariableNames = (1);
+baru = readmatrix('Real_estate.xlsx', opts);
+xlswrite('hasil_wp.xlsx', baru, 'Sheet1', 'A1'); %menulis data pada file colom A1
+V=V'; %merubah data hasil perhitungan dari horizontal ke vertikal matrix
+xlswrite('hasil_wp.xlsx', V, 'Sheet1', 'B1'); %menulis data pada file colom B1
+
+opts = detectImportOptions('hasil_wp.xlsx');
+opts.SelectedVariableNames = (1:2);
+data = readmatrix('hasil_wp.xlsx', opts); %membaca file
+
+X=sortrows(data,2,'descend'); %mengurutkan data dari file berdasar kolom ke-2 dari terbesar
+set(handles.tabel2,'data',X,'visible','on'); %menampilkan data yang telah diurutkan ke tabel
